e7237f22ccea761815e6120ff60809696b3fab04
TheSavageTeddy <51810476+TheSavageTeddy@users.noreply.github.com>
2020-07-27 15:07:04 +0800

    added base64 and 32
    also imported stuff to do later

--- textInfo.py
+++ textInfo.py
@@ -1,2 +1,22 @@
+import base64
+import codecs
+import binascii
+from requests.utils import requote_uri
+from urllib.parse import unquote
+
 text=input("Enter text: ")
 print("Text length:", len(text))
+print('''
+-------------------------------
+            Base 64
+-------------------------------
+''')
+print("Base64 decode (if any): ", base64.decode(text))
+print("Base64 encode (if any): ", base64.encode(text))
+print('''
+-------------------------------
+            Base 32
+-------------------------------
+''')
+print("Base32 decode (if any): ", base32.decode(text))
+print("Base32 encode (if any): ", base32.encode(text))
